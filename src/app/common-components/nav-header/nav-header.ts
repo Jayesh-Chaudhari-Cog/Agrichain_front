d11dc7969b68254b49d8901590dc49c575b4eb53
Thanus Thapasvi <ThanusThapasvi.T@cognizant.com>
2026-05-04 18:35:29 +0530

    Update

--- src/app/common-components/nav-header/nav-header.ts
+++ src/app/common-components/nav-header/nav-header.ts
@@ -1,18 +1,23 @@
-import { Component } from "@angular/core";
+import { Component, inject } from "@angular/core";
 import { AuthService } from "../../services/auth-service";
+import { ROLE_CONFIG, NavLink } from "../../models/nav.model";
+import { RouterLink } from "@angular/router";
+import { LOGIN_INFO } from "../../elements/constants";
+import { LoggedInUser } from "../../models/user.model";
 
 @Component({
     selector: "nav-header",
+    imports: [RouterLink],
     templateUrl: "./nav-header.html",
     styleUrl: "nav-header.css"
 })
 export class NavHeader {
     menuLinks: NavLink[] = [];
-
-    constructor(private authService: AuthService) { }
+    private authService = inject(AuthService);
 
     ngOnInit() {
-        const userRole = localStorage.getItem();
-        this.menuLinks = ROLE_CONFIG[userRole] || [];
+        const loggedInuser = this.authService.loggedInUser();
+        if(loggedInuser && loggedInuser.role)
+            this.menuLinks = ROLE_CONFIG[loggedInuser.role] || [];
     }
 }
