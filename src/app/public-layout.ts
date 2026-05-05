c3175e2d9e464fe15718aca569e28c2ba3ecfb9e
Thanus Thapasvi <ThanusThapasvi.T@cognizant.com>
2026-05-05 18:14:29 +0530

    Toooooooooast

--- src/app/public-layout.ts
+++ src/app/public-layout.ts
@@ -2,15 +2,17 @@ import { Component, signal } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
 import { App } from "./app";
 import { Footer } from './common-components/footer/footer';
+import { ToastComponent } from './common-components/toast/toast';
 
 @Component({
     selector: 'public-layout',
-    imports: [RouterLink, Footer, RouterOutlet],
+    imports: [RouterLink, Footer, RouterOutlet, ToastComponent],
     template: `
     <main>
         <router-outlet />
     </main>
     <app-footer />
+    <app-toast />
   `
 })
 export class PublicLayoutComponent {
