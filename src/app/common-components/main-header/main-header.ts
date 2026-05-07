3cc9da3372de90caa9a96b9e2b40e963cfbfef92
Thanus Thapasvi <ThanusThapasvi.T@cognizant.com>
2026-05-07 11:11:16 +0530

    Update

--- src/app/common-components/main-header/main-header.ts
+++ src/app/common-components/main-header/main-header.ts
@@ -4,15 +4,18 @@ import { WebNameElement } from "../../elements/web-name";
 import { faSignOutAlt } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { AuthService } from '../../services/auth-service';
+import { USER_INFO } from '../../elements/constants';
+import { User } from '../../models/user.model';
 
 @Component({
-  selector: 'app-main-header',
-  imports: [RouterLink, WebNameElement, FontAwesomeModule],
-  templateUrl: './main-header.html',
-  styleUrl: './main-header.css'
+	selector: 'app-main-header',
+	imports: [RouterLink, WebNameElement, FontAwesomeModule],
+	templateUrl: './main-header.html',
+	styleUrl: './main-header.css'
 })
 export class MainHeader {
 	faLogout = faSignOutAlt;
-
 	authService = inject(AuthService);
+
+	user: User = JSON.parse(localStorage.getItem(USER_INFO) || '{}');
 }
