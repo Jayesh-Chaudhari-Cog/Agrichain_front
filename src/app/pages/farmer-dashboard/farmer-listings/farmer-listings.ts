6f6aeb278e63ff2ed0b057b9f643412c8c8a9696
Harini-cog <Harini.S28f23d@cognizant.com>
2026-05-10 17:15:42 +0530

    updating crop listings in farmer

--- src/app/pages/farmer-dashboard/farmer-listings/farmer-listings.ts
+++ src/app/pages/farmer-dashboard/farmer-listings/farmer-listings.ts
@@ -1,19 +1,10 @@
-// import { Component } from '@angular/core';
-
-// @Component({
-//   selector: 'app-farmer-listings',
-//   imports: [],
-//   templateUrl: './farmer-listings.html',
-//   styleUrl: './farmer-listings.css',
-// })
-// export class FarmerListings {}
-
 import { Component, OnInit, inject, signal } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-farmer-listings',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './farmer-listings.html',
   styleUrl: './farmer-listings.css',
 })
@@ -36,3 +27,47 @@ export class FarmerListings implements OnInit {
       });
   }
 }
+
+// import { Component, OnInit, inject, signal } from '@angular/core';
+// import { HttpClient } from '@angular/common/http';
+// import { CommonModule } from '@angular/common';
+// import { AuthService } from '../../../services/auth-service';
+
+// @Component({
+//   selector: 'app-farmer-listings',
+//   standalone: true, 
+//   imports: [CommonModule],
+//   templateUrl: './farmer-listings.html',
+//   styleUrl: './farmer-listings.css',
+// })
+// export class FarmerListings implements OnInit {
+//   private http = inject(HttpClient);
+//   private authService = inject(AuthService);
+  
+//   myListings = signal<any[]>([]);
+
+//   ngOnInit() {
+//     this.fetchMyListings();
+//   }
+
+//   fetchMyListings() {
+//     // Access the signal value by calling it like a function ()
+//     // Use optional chaining (?.) because currentUser could be null
+//     const user = this.authService.currentUser();
+//     const currentFarmerId = user?.id; 
+
+//     if (currentFarmerId) {
+//       this.http.get<any[]>(`http://localhost:8090/market/listings/farmer/${currentFarmerId}`)
+//         .subscribe({
+//           next: (data) => {
+//             this.myListings.set(data);
+//           },
+//           error: (err) => {
+//             console.error("Failed to load listings:", err);
+//           }
+//         });
+//     } else {
+//       console.error("No logged-in user found in AuthService signals!");
+//     }
+//   }
+// }
